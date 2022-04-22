ad6b7acf3aab7fa3aad0eee97e7df44d5f887b85
Verna Leisti <verna.leisti@tuni.fi>
2022-04-22 23:52:06 +0300

    Revert "Merge branch 'version_2.1' into 'main'"
    This reverts merge request !3

--- source/datapresenter/controller.cpp
+++ source/datapresenter/controller.cpp
@@ -1,28 +1,6 @@
 #include "controller.hh"
-#include <QDebug>
 
-Controller::Controller(Model *model, QObject *parent)
-    : QObject{parent}
-    , model_{model}
+Controller::Controller(Model* model):
+    model_{model}
 {
-}
-
-void Controller::showPreferences()
-{
-    model_->showPreferences();
-}
-
-void Controller::saveOptionsToPreferences()
-{
-    model_->saveToPreferences();
-}
-
-void Controller::showStatistics()
-{
-    model_->showStatistics();
-}
-
-void Controller::updateCardArea()
-{
-    model_->updateCardArea();
 }
--- source/datapresenter/main.cpp
+++ source/datapresenter/main.cpp
@@ -1,31 +1,17 @@
 #include "mainwindow.hh"
 #include "model.hh"
 #include "controller.hh"
-
 #include <QApplication>
-#include <QFile>
-#include "concretestatfi.hh"
-
-#include "concretesmear.hh"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // Load an application style
-    QFile styleFile( "stylesheet.qss" );
-    styleFile.open( QFile::ReadOnly );
+    MainWindow* view = new MainWindow();
+    Model* model = new Model(view);
+    Controller* controller = new Controller(model);
 
-    // Apply the loaded stylesheet
-    QString style = QLatin1String( styleFile.readAll() );
-    a.setStyleSheet( style );
-
-    Model* model{new Model()};
-    Controller* controller{new Controller(model)};
-    MainWindow* view{new MainWindow(controller)};
-
-    model->setView(view);
-    model->setupView();
+    view->setController(controller);
     view->show();
 
     return a.exec();
